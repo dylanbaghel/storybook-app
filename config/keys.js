aff2e1565875b564bb284d9e6f7bc9d45a46586d
dylanbaghel <baghelabhi39@gmail.com>
2018-08-26 22:59:11 +0530

    patch for oauth auth for remote

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    googleClientID: '487119560492-rbjf4tb15jnke79hdhvm37bgt678f65i.apps.googleusercontent.com',
-    googleClientSecret: 'Ggwmp2wiL5n9KyyAOkgDCbVF'
+    googleClientID: '718600670248-1p3ikhmjdposd9k6i3cetkgesthqc802.apps.googleusercontent.com',
+    googleClientSecret: 'jYT8JJppdxPKmU4J-dNTOOsj'
 };
